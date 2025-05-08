96d61d8043742802e6eab66f1c1ef32b6e00fa0b
Dark hacker 😈 <javieldev@gmail.com>
2025-05-08 23:17:06 +0300

    Create python sms bomber.py

---
+++ python sms bomber.py
@@ -0,0 +1,2 @@
+## Disclaimer:
+This tool is for educational purposes only. Always get permission before testing with someone else's number. Malicious use of this tool is illegal and can lead to serious consequences.
